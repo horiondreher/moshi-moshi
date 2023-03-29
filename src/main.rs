044703a4b3cbe8acecbe23cbe89dd98818beadb4
horiondreher <horiondreher@gmail.com>
2023-03-29 18:39:14 -0300

    Succesfully parsers most of SIP values

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use nom::{
-    bytes::complete::take_while,
-    character::complete::{char, space0},
+    branch::alt,
+    bytes::complete::{is_not, tag, take_until, take_while},
+    character::{
+        complete::{alphanumeric0, char, digit0, space0},
+    },
     combinator::map,
-    sequence::{delimited, separated_pair},
-    IResult,
+    error::{Error, ErrorKind, ParseError},
+    sequence::{delimited, separated_pair, tuple},
+    IResult, Parser,
 };
 
-use std::collections::{BTreeMap, VecDeque};
-use std::str;
-use std::{env, net};
+#[derive(Debug, Clone)]
+struct SipParseError;
 
-// use nom::bytes::complete::{take_while, take_while1};
-
-#[macro_export]
-macro_rules! sip_parse_error {
-    // error with message
-    ($error_code:expr) => {
-        Err(nom::Err::Error(SipLineParseError::new($error_code, None)))
-    };
-
-    // error without message
-    ($error_code:expr, $message:expr) => {
-        Err(nom::Err::Error(SipLineParseError::new(
-            $error_code,
-            Some($message),
-        )))
-    };
+impl fmt::Display for SipParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid SIP message format")
+    }
 }
+
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::str;
+use std::{
+    collections::{BTreeMap, VecDeque},
+    fmt,
+};
+use std::{env, net};
 
 #[derive(Debug)]
 enum ReqMethod {
@@ -69,13 +68,48 @@ enum ReqDirection {
     Out,
 }
 
+pub fn is_any_ascii(input: &str) -> IResult<&str, &str> {
+    take_while(|c: char| c.is_ascii())(input)
+}
+
+pub fn ascii(input: &str) -> IResult<&str, &str> {
+    take_while(|c: char| c.is_ascii())(input)
+}
+
+fn not_space_or_colon(s: &str) -> IResult<&str, &str> {
+    is_not(" \t\r\n:")(s)
+}
+
 struct Uri<'a> {
-    raw: &'a str,
-    display_name: &'a str,
-    protocol: &'a str,
+    display_name: Option<&'a str>,
+    scheme: &'a str,
     user: &'a str,
-    address: &'a str,
-    tag: &'a str,
+    host: &'a str,
+    port: u16,
+    params: Option<BTreeMap<&'a str, Param<'a>>>,
+}
+
+impl<'a> Uri<'a> {
+    /// First this function split the scheme part of URI (sip or sips) using alt, if fails for sip it tries for sips
+    /// Second it uses separated_pair to break apart the user from host and port, using '@' for it
+    /// Finally split the host and port to store in variables
+    fn parse(input: &'a str) -> IResult<&str, Self> {
+        let (input, scheme) = alt((tag("sip:"), tag("sips:")))(input)?;
+        let (_params, (user, host_port)) = separated_pair(alphanumeric0, char('@'), is_any_ascii)(input)?;
+        let (output, (host, port)) = separated_pair(take_until(":"), char(':'), digit0)(host_port)?;
+
+        Ok((
+            output,
+            Uri {
+                display_name: None,
+                user,
+                scheme,
+                host: host,
+                port: port.parse::<u16>().unwrap(),
+                params: None,
+            },
+        ))
+    }
 }
 
 struct Param<'a> {
@@ -106,57 +140,84 @@ struct Headers<'a> {
 struct ReqMessage<'a> {
     method: ReqMethod,
     direction: ReqDirection,
-    orig_addr: &'a str,
     uri: Uri<'a>,
+    version: &'a str,
     headers: Option<Headers<'a>>,
-    body: Option<&'a str>,
+    body: Option<Vec<&'a str>>,
 }
 
-// pub fn is_field_char(c: char) -> bool {
-//     c.is_ascii() && c != ':'
-// }
 
-pub fn is_field_char(input: &str) -> IResult<&str, &str> {
-    take_while(|c: char| c.is_ascii() && c != ':')(input)
-}
 
 impl<'a> ReqMessage<'a> {
-    fn new(message: &str) -> Self {
-        let lines: Vec<&str> = message.split("\r\n").collect();
+    fn parse(message: &str) -> Result<ReqMessage, SipParseError> {
+        let mut lines: Vec<&str> = message.split("\r\n").collect();
+        let mut headers: Headers;
 
-        for line in lines {
-            println!("{}", line);
-            if let Ok((_input, header)) = ReqMessage::parse_line(line) {
-                println!("Header - {}", header.name);
-                println!("Value - {}", header.value);
+        let uri_line = lines.remove(0);
+        let (method, uri, version) = match ReqMessage::parse_request_uri(uri_line) {
+            Ok((_, (method, uri, version))) => (method, uri, version),
+            Err(e) => {
+                println!("Error: {}", e);
+                return Err(SipParseError);
             }
+        };
+
+        let ending =  lines.iter().position(|&x| x == "");
+        let body_values = match ending {
+            Some(end_pos) => Some(lines.drain(end_pos..).collect()),
+            None => None
+        };
+
+        for line in &lines {
+            println!("{}", line);
+            let (_input, header) = match ReqMessage::parse_header(line) {
+                Ok((_input, header)) => {
+                    println!("Header - {}", header.name);
+                    println!("Value - {}\n", header.value);
+                    (_input, header)
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                    continue;
+                }
+            };
         }
 
-        ReqMessage {
+        Ok(ReqMessage {
             method: ReqMethod::Invite,
             direction: ReqDirection::In,
-            orig_addr: "",
-            uri: Uri {
-                raw: "",
-                display_name: "",
-                protocol: "",
-                user: "",
-                address: "",
-                tag: "",
-            },
+            uri: uri,
+            version: version,
             headers: None,
-            body: None,
-        }
+            body: body_values,
+        })
     }
 
-    fn parse_line(line: &str) -> IResult<&str, Header> {
-        let parse_header_value = separated_pair(
-            is_field_char,
-            delimited(space0, char(':'), space0),
-            take_while(|c: char| c.is_ascii()),
-        );
+    /// This parser works on the first line of a SIP message 
+    /// It separates method (like INVITE), destination URI 
+    fn parse_request_uri(line: &str) -> IResult<&str, (&str, Uri, &str)> {
+        let (version, (method, raw_uri)) = separated_pair(alphanumeric0, space0, take_until(" "))(line)?;
+        let (output, uri) = Uri::parse(raw_uri)?;
 
-        map(parse_header_value, |(header, value)| Header {
+        Ok((output, (method, uri, version)))
+    }
+
+    /// This parser break apart Header from Values using separated_pair
+    /// Example - Via: SIP/2.0/UDP 192.168.1.146:5070;branch=z9hG4bK-10149-1-0
+    ///
+    /// The first parameter is a parser that gets the values until its not space or color (as the name implies)
+    /// but the reason of it is because: "The formal grammar for a message-header specified in Section 25
+    /// allows for an arbitrary amount of whitespace on either side of the colon;" (RFC 3261)
+    ///
+    /// The second parameter is a parser that gets the caracters that will be discarded,
+    /// so it can be zero or more spaces, a colon, and zero or more spaces again
+    ///
+    /// The third paramter gets any ascii caracter until the end of the line
+    fn parse_header(line: &str) -> IResult<&str, Header> {
+        let header_value = separated_pair(not_space_or_colon, tuple((space0, tag(":"), space0)), ascii);
+
+        // Transforms the parser into a Header struct
+        map(header_value, |(header, value)| Header {
             name: header,
             value: value,
             params: None,
@@ -165,13 +226,13 @@ impl<'a> ReqMessage<'a> {
 }
 fn main() {
     let host_addr = env::args().nth(1).expect("Invalid host IP address");
-    let host_port = env::args().nth(2).expect("Invalid host port");
-    let host = format!("{}:{}", host_addr, host_port);
+    let port = env::args().nth(2).expect("Invalid port");
+    let addrs = format!("{}:{}", host_addr, port);
 
     let mut buf = vec![0; 2048];
 
-    println!("Bindind socket on {}...", host);
-    let socket = match net::UdpSocket::bind(&host) {
+    println!("Bindind socket on {}...", addrs);
+    let socket = match net::UdpSocket::bind(&addrs) {
         Ok(s) => s,
         Err(e) => panic!("Could not bind to socket. Reason: {}", e),
     };
@@ -184,7 +245,7 @@ fn main() {
                 match str::from_utf8(&buf) {
                     Ok(valid) => {
                         // println!("{}", valid);
-                        let message = ReqMessage::new(valid);
+                        let message = ReqMessage::parse(valid);
                     }
                     Err(error) => {
                         println!("Invalid received bytes: {}", error.to_string());
